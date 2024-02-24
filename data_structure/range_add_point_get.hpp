90199a03b5ccb2d334c732223b406cd57ddd066d
Forested <ryotaroh2006@gmail.com>
2024-02-24 21:55:02 +0900

    fix name

--- data_structure/range_add_point_get.hpp
+++ data_structure/range_add_point_get.hpp
@@ -3,12 +3,12 @@
 #include "fenwick_tree.hpp"
 
 template <typename T>
-class RangeAddRangeSum {
+class RangeAddPointGet {
     int n;
     FenwickTree<Add<T>> ft;
 
 public:
-    RangeAddRangeSum(int n) : n(n), ft(n + 1) {}
+    RangeAddPointGet(int n) : n(n), ft(n + 1) {}
 
     void add(int l, int r, const T &v) {
         assert(0 <= l && l <= r && r <= n);
@@ -16,7 +16,7 @@ public:
         ft.add(r, -v);
     }
 
-    T sum(int idx) const {
+    T get(int idx) const {
         assert(0 <= idx && idx < n);
         return ft.sum(idx + 1);
     }

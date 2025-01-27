f7b598e0d8c8c3624a0248725e265725b10e5d3f
Forested <ryotaroh2006@gmail.com>
2025-01-27 21:46:44 +0900

    optimize factorize 3

--- number_theory/test/factorize.test.cpp
+++ number_theory/test/factorize.test.cpp
@@ -1,21 +1,24 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/factorize"
 #define FAST_IO
 #include "../../number_theory/factorize.hpp"
+#include "../../template/fastio.hpp"
 #include "../../template/template.hpp"
 
 void solve() {
-    U64(n);
+    u64 n;
+    rd.read(n);
     V<u64> ps = factorize(n);
-    cout << LEN(ps);
+    wr.write(LEN(ps));
     for (i64 p : ps) {
-        cout << ' ' << p;
+        wr.write(' ');
+        wr.write(p);
     }
-    cout << '\n';
+    wr.writeln();
 }
 
 int main() {
     i32 t = 1;
-    cin >> t;
+    rd.read(t);
     while (t--) {
         solve();
     }
